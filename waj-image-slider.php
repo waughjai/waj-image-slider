305626d092360c6f1f7a033160d8634384664c54
Jaimeson Waugh <jaimeson@4cesi.com>
2019-01-03 12:17:55 -0800

    Get rid o' error code & update classes

--- waj-image-slider.php
+++ waj-image-slider.php
@@ -13,8 +13,6 @@
 
 	namespace WaughJ\WPImageSlider
 	{
-		error_reporting( E_ALL );
-		ini_set( 'display_errors', 1 );
 		require_once( 'vendor/autoload.php' );
 
 		use WaughJ\HTMLImageSlider\HTMLImageSlider;
